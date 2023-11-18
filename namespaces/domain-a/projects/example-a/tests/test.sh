80fdd21143144b1099af14de38b02ce4f4335de2
mdolinin <dmo.builder@gmail.com>
2023-11-18 16:09:07 -0500

    feat(useful): add some work

---
+++ namespaces/domain-a/projects/example-a/tests/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Hello from example-a"
+echo "Usefull work"
+echo "This is a test script"
---
+++ namespaces/domain-a/projects/example-b/tests/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Hello from example-b"
+echo "This is a test script"
